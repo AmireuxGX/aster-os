d46c46824f91b31ec9114a488eeb4f840c5a9304
AmireuxGX <1113533973@QQ.COM>
2026-08-18 02:40:59 +0800

    Add src/apps/index.tsx

---
+++ src/apps/index.tsx
@@ -0,0 +1,13 @@
+import './apps.css'
+
+import type { AppId } from '../types'
+import { appDefinitions } from './AppSuite'
+
+export { appDefinitions }
+
+export function getAppDefinition(appId: AppId) {
+  const definition = appDefinitions.find((app) => app.id === appId)
+  if (!definition) throw new Error(`Unknown app: ${appId}`)
+  return definition
+}
+
